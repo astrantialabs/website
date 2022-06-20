ed70b8904310c52ee2739de805d9b8b89c9cbf7c
yehezkieldio <yehezkieldio35@gmail.com>
2022-06-20 09:57:27 +0800

    chore: static export

---
+++ out/_next/static/HjxDnRh6ttyOCg2Wnki_1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/759-8073266e8aafb50c.js","static/chunks/pages/index-3df0d35c6df6cc07.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
